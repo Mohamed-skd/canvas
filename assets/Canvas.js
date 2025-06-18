6cbb3876a12a12f32122f56e6a6bb79db666033d
Mohamed <contactdiabysn@gmail.com>
2025-06-18 23:20:10 +0200

    c1 & c2

--- assets/Canvas.js
+++ assets/Canvas.js
@@ -28,18 +28,18 @@ export class Circle {
   }
 
   draw(fill=false, color= [100, 100, 100]){
-    this.ctx.beginPath()
+    const circle= ()=>this.ctx.arc(...this.center, this.radius, 0, Math.PI * 2)
     
+    this.ctx.beginPath()
     if (fill) {
       this.ctx.fillStyle= this.#getColor(color)
-      this.ctx.arc(...this.center, this.radius, 0, Math.PI * 2)
+      circle()
       this.ctx.fill()
     } else {
       this.ctx.strokeStyle= this.#getColor(color)
-      this.ctx.arc(...this.center, this.radius, 0, Math.PI * 2)
+      circle()
       this.ctx.stroke()
     }
-
     this.ctx.closePath()
   }
 
@@ -48,9 +48,97 @@ export class Circle {
   }
 }
 
-export class Square {}
+export class Square {
+  constructor(ctx, topLeftCorner, size){
+    this.ctx= ctx
+    this.start= topLeftCorner
+    this.size= size
+  }
 
-export class Triangle {}
+  draw(fill=false, color= [100, 100, 100]){
+    const square= ()=> this.ctx.rect(this.start[0], this.start[1], this.size, this.size)
 
-export class Path {}
+    this.ctx.beginPath()
+    if (fill) {
+      this.ctx.fillStyle= this.#getColor(color)
+      square()
+      this.ctx.fill()
+    } else {
+      this.ctx.strokeStyle= this.#getColor(color)
+      square()
+      this.ctx.stroke()
+    }
+    this.ctx.closePath()
+  }
+
+  #getColor(color){
+    return `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+  }
+}
+
+export class Triangle {
+  constructor(ctx, bottomLeftCorner, size){
+    this.ctx= ctx
+    this.start= bottomLeftCorner
+    this.size= size
+  }
+
+  draw(fill= false, color= [100, 100, 100]){
+    const triangle= ()=>{
+      let nX= this.start[0]
+      let nY= this.start[1]
+      let angle= 0
+      this.ctx.moveTo(...this.start)
+
+      for (let i=0; i<3; i++){
+	const x= nX + this.size * Math.cos(angle * Math.PI/180);
+	const y= nY + this.size * Math.sin(angle * Math.PI/180);
+	this.ctx.lineTo(x, y);
+
+	nX= x
+	nY= y
+	angle-= 120;
+      }
+    }
+
+    this.ctx.beginPath()
+    if (fill) {
+      this.ctx.fillStyle= this.#getColor(color)
+      triangle()
+      this.ctx.fill()
+    } else {
+      this.ctx.strokeStyle= this.#getColor(color)
+      triangle()
+      this.ctx.stroke()
+    }
+    this.ctx.closePath()
+  }
+
+  #getColor(color){
+    return `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+  }
+}
+
+export class Path {
+  constructor(ctx, path){
+    this.ctx= ctx
+    this.path= path
+  }
+
+  draw(width= 1, color= [100, 100, 100]){
+    this.ctx.beginPath()
+    this.ctx.strokeStyle= this.#getColor(color)
+    this.ctx.lineWidth= width
+    this.ctx.moveTo(...this.path[0])
+    for (let i=1; i<this.path.length; i++){
+      this.ctx.lineTo(...this.path[i])
+    }
+    this.ctx.stroke()
+    this.ctx.closePath()
+  }
+
+  #getColor(color){
+    return `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+  }
+}
 
--- assets/script.js
+++ assets/script.js
@@ -16,22 +16,44 @@ const ctx1= c1.getContext("2d")
 const width= c1.width
 const height= c1.height
 
-const line= new Line(ctx1, [100, 100], [500, 150])
+const line= new Line(ctx1, [100, 50], [500, 50])
 line.draw()
-
-const circle= new Circle(ctx1, [200, 200], 50)
+const circle= new Circle(ctx1, [200, 300], 50)
 circle.draw()
+const square= new Square(ctx1, [250, 100], 100)
+square.draw()
+const triangle= new Triangle(ctx1, [400,300], 100)
+triangle.draw(true)
+const path= new Path(ctx1, [
+  [50, 110],
+  [180, 150],
+  [50, 300],
+  [80, 380],
+  [300, 380],
+  [350, 260],
+  [450, 120],
+  [580, 350]
+])
+path.draw(5)
 
 // canvas 2
 const c2= domFn.select("#c2")
 const ctx2= c2.getContext("2d")
 const center= [c2.width/2, c2.height/2]
+const pts= []
 let angle= 0
 
-for (let i= 0; i< 300; i++){
+for (let i= 0; i< 300; i+= 0.2){
   const x= center[0] + i * Math.cos(angle);
   const y= center[1] + i * Math.sin(angle);
-  angle++;
+  angle-= 0.05;
 
-  (new Circle(ctx2, [x,y], 50)).draw()
+  pts.push([x, y])
 }
+const spirale= new Path(ctx2, pts)
+spirale.draw()
+
+// canvas 3
+const c3= domFn.select("#c3")
+const ctx3= c3.getContext("2d")
+
