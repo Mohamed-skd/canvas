57ae926678201bc3f9e227bfc9d18c1c7ed3638c
Mohamed <contactdiabysn@gmail.com>
2025-06-26 15:53:58 +0200

    fx

--- pwa.js
+++ pwa.js
@@ -1,6 +1,6 @@
-const cacheName= "app-cache-2"
-const cacheFiles= [
-"",
+importScripts("dist/scripts/SW.js")
+
+const pwa= new PWA("canvas-cache", [
 "index.html",
 "detail.html",
 "detail.html?id=n1",
@@ -17,60 +17,9 @@ const cacheFiles= [
 "dist/script.js",
 "dist/detail.js",
 "dist/style.css",
-]
+])
 
-async function setCache(){
-  try {
-    const cache= await caches.open(cacheName)
-    return cache.addAll(cacheFiles)
-  } catch (err) {
-    console.log(err)
-    return false
-  }
-}
-function install(e){
-  e.waitUntil(setCache())
-  console.log(`${cacheName} installed !`)
-}
-
-async function cleanCache(){
-  try {
-    const keys= await caches.keys()
-
-    return Promise.all(keys.map(k=>{
-      if (k!==cacheName) return caches.delete(k)
-      return true
-    }))
-  } catch (err) {
-    console.log(err)
-    return false
-  }
-}
-function cleanHist(e){
-  e.waitUntil(cleanCache())
-  console.log("Former cache cleaned !")
-}
-
-async function getCache(event){
-  try {
-    const res= await caches.match(event.request)
-    if (res) return res
-
-    const response= await fetch(event.request)
-    const cache= await caches.open(cacheName)
-    cache.put(event.request, response.clone())
-    return response
-  } catch (err) {
-    console.log(err)
-    return false
-  }
-}
-function history(e){
-  e.respondWith(getCache(e))
-  console.log("Cache response.")
-}
-
-self.addEventListener("install", install)
-self.addEventListener("activate", cleanHist)
-self.addEventListener("fetch", history)
+addEventListener("install", pwa.setCache.bind(pwa))
+addEventListener("activate", pwa.cleanCache.bind(pwa))
+addEventListener("fetch", pwa.getCache.bind(pwa))
 
